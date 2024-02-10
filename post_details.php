a710c3227bae40103e4a7ba9daecb349aecf1a15
susanthakuri92 <susanthakuri92@gmail.com>
2024-02-10 16:33:19 +0545

    susan hero

--- post_details.php
+++ post_details.php
@@ -347,7 +347,7 @@
                     return "";
                 }
             }
-        }
+        }                                                                                            
         ?>
     </main>
     <footer>
